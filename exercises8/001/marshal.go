734d0e17a9cc6c412951ab85cf77a647372a352f
Manuel Benavides <manuel@bybsoft.com>
2019-11-08 17:41:43 -0600

    exercises 8

---
+++ exercises8/001/marshal.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	fmt.Println(users)
+
+	bs, error := json.Marshal(users)
+	if error != nil {
+		fmt.Println(error)
+	}
+	fmt.Println(string(bs))
+
+}
